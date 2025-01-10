7fae8797ef4f14cc7879ffd357a1870e339878c9
Fiqri Ismail <fiqri.ismail@gmail.com>
2025-01-10 22:23:38 +0530

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Fiqri Ismail on 2025-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("You Are Awesome!")
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Fiqri Ismail on 2025-01-10.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
