93f8edeee4254e83ae1806fd5ce35f73bf27f0d4
Fiqri Ismail <fiqri.ismail@gmail.com>
2025-01-13 16:49:43 +0530

    Added more buttons with formatting. Added spacers and frame

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "Awesome!"
+    @State private var message = "I am a programmer"
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Cick me!") {
-                message = "You clicked me!"
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great") {
+                    message = "Great!"
+                }
+                
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
             
         }
         .padding()
